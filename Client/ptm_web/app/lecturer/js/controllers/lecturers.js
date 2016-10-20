22e4fdf5aa81b9091c208ca679aab6fc3b7fa9e0
DewmiR <dewmirandika@gmail.com>
2016-10-20 15:16:31 +0530

    Assign Multiple lecturers/supervisors for a particular module

--- Client/ptm_web/app/lecturer/js/controllers/lecturers.js
+++ Client/ptm_web/app/lecturer/js/controllers/lecturers.js
@@ -2,7 +2,7 @@
  * Created by DewmiR on 9/26/2016.
  */
 
-myApp.controller('lecturerController', ['$scope','$http','$location', function($scope,$http,$location) {
+lectApp.controller('lecturerController', ['$scope','$http','$location', function($scope,$http,$location) {
 
 
     $scope.init = function () {
@@ -33,13 +33,13 @@ myApp.controller('lecturerController', ['$scope','$http','$location', function($
             url:'/getAllLecturers'
         }).then(
             function success(response) {
-                console.log(response.data);
+                //console.log(response.data);
                 Array.prototype.push.apply($scope.lecturers, response.data);
-                console.log($scope.lecturers);
+                //console.log($scope.lecturers);
 
             },
             function error(error) {
-                console.log('Failed to load Lecturers');
+               // console.log('Failed to load Lecturers');
             }
         );
 
@@ -51,13 +51,13 @@ myApp.controller('lecturerController', ['$scope','$http','$location', function($
             url:'/displayAllModules'
         }).then(
             function success(response) {
-                console.log(response.data);
+                //console.log(response.data);
                 Array.prototype.push.apply($scope.modules, response.data);
-                console.log($scope.modules);
+               // console.log($scope.modules);
 
             },
             function error(error) {
-                console.log('Failed to load modules');
+               // console.log('Failed to load modules');
             }
         );
 
@@ -66,37 +66,54 @@ myApp.controller('lecturerController', ['$scope','$http','$location', function($
 
     $scope.update = function() {
         slectedCourse=$scope.item.courseName;
-        console.log($scope.item.courseName);
-        console.log(slectedCourse);
+       // console.log($scope.item.courseName);
+       // console.log(slectedCourse);
 
 
     };
 
     $scope.updateLec = function() {
         selectedPost=$scope.lPost.post;
-        console.log($scope.lPost.post);
-        console.log(selectedPost);
+      //  console.log($scope.lPost.post);
+       // console.log(selectedPost);
 
 
     };
 
 
-    $scope.getName = function () {
+   /* $scope.getName = function () {
             lname=$scope.lec.name;
             console.log(lname)
 
-    };
+    };*/
 
 
     $scope.assignLecs = function() {
 
+        //get all selected lecturers
+        $scope.getSelectedLecturer = function(){
+            $scope.nameArr = [];
+            angular.forEach($scope.lecturers, function(lecturer){
+                if (lecturer.selected) $scope.nameArr.push(lecturer.name);
+                //console.log( $scope.nameArr);
+            });
+        };
+
+
+        $scope.getSelectedLecturer();
+
+       //for each selected lecturer , add a new record to db
+        angular.forEach($scope.nameArr, function(value) {
+            console.log(value);
+
         $http.post('/assignLecturer', {
             courseName: slectedCourse,
-            userName : lname,
+            userName : value,
             post : selectedPost
 
         }).success(
             function(data){
+               $scope.displayAsgnDetails();
                console.log(data);
             }
         ).error(
@@ -104,6 +121,8 @@ myApp.controller('lecturerController', ['$scope','$http','$location', function($
                 console.log(error);
             }
         );
+
+       });
 
     };
 
@@ -116,7 +135,6 @@ myApp.controller('lecturerController', ['$scope','$http','$location', function($
                 console.log(response.data);
                 Array.prototype.push.apply($scope.modules, response.data);
                 console.log($scope.modules);
-
             },
             function error(error) {
                 console.log('Failed to load modules');
@@ -132,9 +150,9 @@ myApp.controller('lecturerController', ['$scope','$http','$location', function($
             url:'/getAllAssigenedLecturers'
         }).then(
             function success(response) {
-                console.log(response.data);
+                //console.log(response.data);
                 Array.prototype.push.apply($scope.details, response.data);
-                console.log($scope.details);
+               // console.log($scope.details);
 
             },
             function error(error) {
